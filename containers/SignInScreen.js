cdc2fd17842896cb274d4d6ef63c6c62ae7268a8
Athenaïs Razafindramonja <athenais.raza@gmail.com>
2021-05-19 15:57:17 +0200

    sign in

--- containers/SignInScreen.js
+++ containers/SignInScreen.js
@@ -12,48 +12,76 @@ import {
 
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-export default function SignInScreen({ setToken }) {
-  const navigation = useNavigation();
-  return (
-    <KeyboardAwareScrollView>
-      <View style={styles.header}>
-        <Image
-          style={styles.logo}
-          source={require("../assets/logo.png")}
-          resizeMode="contain"
-        />
-        <Text style={styles.txt}>Sign in</Text>
-      </View>
-      <View style={styles.signIn}>
-        <TextInput style={styles.email} placeholder="Email" />
-        <TextInput
-          style={styles.pwd}
-          placeholder="Password"
-          secureTextEntry={true}
-        />
-        <TouchableOpacity
-          style={styles.btn}
-          title="Sign in"
-          onPress={async () => {
-            const userToken = "secret-token";
-            setToken(userToken);
-          }}
-        >
-          <Text style={styles.btnText}>Sign in</Text>
-        </TouchableOpacity>
+import { useState } from "react";
+import axios from "axios";
 
-        <TouchableOpacity
-          style={styles.btnRegister}
-          onPress={() => {
-            navigation.navigate("SignUp");
-          }}
-        >
-          <Text style={styles.txtRegister}>No account? Register</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAwareScrollView>
-  );
+export default function SignInScreen({ setToken }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "https://express-airbnb-api.herokuapp.com/user/log_in",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      console.log(response.data);
+      if (response.status === 200) {
+        alert("You are connected");
+      } else if (error.response.status === 401) {
+        setError("Email and/or password is wrong");
+      }
+    } catch (error) {
+      console.log(error.response.data.error);
+      setError("Unauthorized");
+    }
+  };
 }
+
+const navigation = useNavigation();
+return (
+  <KeyboardAwareScrollView>
+    <View style={styles.header}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/logo.png")}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Sign in</Text>
+    </View>
+    <View style={styles.signIn}>
+      <TextInput style={styles.input} placeholder="Email" />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        title="Sign in"
+        onPress={async () => {
+          const userToken = "secret-token";
+          setToken(userToken);
+        }}
+      >
+        <Text style={styles.btnText}>Sign in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.btnRegister}
+        onPress={() => {
+          navigation.navigate("SignUp");
+        }}
+      >
+        <Text style={styles.txtRegister}>No account? Register</Text>
+      </TouchableOpacity>
+    </View>
+  </KeyboardAwareScrollView>
+);
 
 const styles = StyleSheet.create({
   header: {
@@ -67,7 +95,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
 
-  txt: {
+  title: {
     fontSize: 26,
     fontWeight: "bold",
     textAlign: "center",
@@ -75,20 +103,14 @@ const styles = StyleSheet.create({
     marginBottom: 50,
   },
 
-  email: {
+  signIn: {
+    padding: 20,
+  },
+
+  input: {
     borderBottomWidth: 1,
     borderBottomColor: "#F8585B",
     marginBottom: 40,
-  },
-
-  pwd: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#F8585B",
-    marginBottom: 150,
-  },
-
-  signIn: {
-    padding: 20,
   },
 
   btn: {
@@ -97,6 +119,7 @@ const styles = StyleSheet.create({
     borderColor: "#F8585B",
     borderRadius: 50,
     padding: 15,
+    marginTop: 150,
   },
 
   btnText: {
--- containers/SignUpScreen.js
+++ containers/SignUpScreen.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View, Image, StyleSheet } from "react-native";
+
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function SignUpScreen({ setToken }) {
   return (
-    <View>
-      <View>
-        <Text>Name: </Text>
-        <TextInput placeholder="Username" />
-        <Text>Password: </Text>
-        <TextInput placeholder="Password" secureTextEntry={true} />
+    <KeyboardAwareScrollView>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/logo.png")}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>Sign up</Text>
+      </View>
+      <View style={styles.SignUp}>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Username" />
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Describe yourself in a few words..."
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm password"
+          secureTextEntry={true}
+        />
         <Button
           title="Sign up"
           onPress={async () => {
@@ -17,6 +39,44 @@ export default function SignUpScreen({ setToken }) {
           }}
         />
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+  },
+
+  logo: {
+    width: 150,
+    height: 100,
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "grey",
+  },
+
+  signUp: {
+    padding: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#F8585B",
+    marginBottom: 40,
+  },
+
+  inputBox: {
+    height: 100,
+    width: "100%",
+    borderColor: "#F8585B",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    textAlignVertical: "top",
+  },
+});
